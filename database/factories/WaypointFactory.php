be10469fa8d596cc8f693af61d8743dea626ace8
Michele Bonacina <michele.bonacina@gmail.com>
2020-08-31 20:26:24 +0200

    added waypoint model

---
+++ database/factories/WaypointFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Waypoint;
+use Faker\Generator as Faker;
+
+$factory->define(Waypoint::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
