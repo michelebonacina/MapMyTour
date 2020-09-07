5a0d4c4b07b537e4566447d6e377eae8867883c8
Michele Bonacina <michele.bonacina@gmail.com>
2020-09-07 15:24:28 +0200

    mantained form values on errors

--- resources/views/waypoints/create.blade.php
+++ resources/views/waypoints/create.blade.php
@@ -11,27 +11,27 @@
                 @csrf
                 <div class="form-group">
                     <label for="name">Name</label>
-                    <input type="text" required class="form-control" id="name" name="name" autofocus
-                        aria-describedby="nameHelp">
+                    <input type="text" required class="form-control" id="name" name="name" value="{{old('name')}}"
+                        autofocus aria-describedby="nameHelp">
                     <small id="nameHelp" class="form-text text-muted">The name of the waypoint.</small>
                 </div>
                 <div class="form-group">
                     <label for="latitude">Latitude</label>
                     <input type="number" step="any" min="-90" max="90" required class="form-control" id="latitude"
-                        name="latitude" aria-describedby="latitudeHelp">
+                        name="latitude" value="{{old('latitude')}}" aria-describedby="latitudeHelp">
                     <small id="latitudeHelp" class="form-text text-muted">The latitude coordinate of the waypoint.
                         Between -90 and +90.</small>
                 </div>
                 <div class="form-group">
                     <label for="longitude">Longitude</label>
                     <input type="number" step="any" min="-180" max="180" required class="form-control" id="longitude"
-                        name="longitude" aria-describedby="longitudeHelp">
+                        name="longitude" value="{{old('longitude')}}" aria-describedby="longitudeHelp">
                     <small id="longitudeHelp" class="form-text text-muted">The longitude coordinate of the waypoint.
                         Between -180 and +180.</small>
                 </div>
                 <div class="form-group">
                     <label for="altitude">Altitude</label>
-                    <input type="number" class="form-control" id="altitude" name="altitude"
+                    <input type="number" class="form-control" id="altitude" name="altitude" value="{{old('altitude')}}"
                         aria-describedby="altitudeHelp">
                     <small id="altitudeHelp" class="form-text text-muted">The altitude of the waypoint.</small>
                 </div>
--- resources/views/waypoints/edit.blade.php
+++ resources/views/waypoints/edit.blade.php
@@ -14,27 +14,27 @@
                 <div class="form-group">
                     <label for="name">Name</label>
                     <input type="text" required class="form-control" id="name" name="name" autofocus
-                        aria-describedby="nameHelp" value="{{$waypoint->name}}">
+                        aria-describedby="nameHelp" value="{{old('name', $waypoint->name)}}">
                     <small id="nameHelp" class="form-text text-muted">The name of the waypoint.</small>
                 </div>
                 <div class="form-group">
                     <label for="latitude">Latitude</label>
                     <input type="number" step="any" min="-90" max="90" required class="form-control" id="latitude"
-                        name="latitude" aria-describedby="latitudeHelp" value="{{$waypoint->latitude}}">
+                        name="latitude" aria-describedby="latitudeHelp" value="{{old('latitude', $waypoint->latitude)}}">
                     <small id="latitudeHelp" class="form-text text-muted">The latitude coordinate of the waypoint.
                         Between -90 and +90.</small>
                 </div>
                 <div class="form-group">
                     <label for="longitude">Longitude</label>
                     <input type="number" step="any" min="-180" max="180" required class="form-control" id="longitude"
-                        name="longitude" aria-describedby="longitudeHelp" value="{{$waypoint->longitude}}">
+                        name="longitude" aria-describedby="longitudeHelp" value="{{old('longitude', $waypoint->longitude)}}">
                     <small id="longitudeHelp" class="form-text text-muted">The longitude coordinate of the waypoint.
                         Between -180 and +180.</small>
                 </div>
                 <div class="form-group">
                     <label for="altitude">Altitude</label>
                     <input type="number" class="form-control" id="altitude" name="altitude"
-                        aria-describedby="altitudeHelp" value="{{$waypoint->altitude}}">
+                        aria-describedby="altitudeHelp" value="{{old('altitude', $waypoint->altitude)}}">
                     <small id="altitudeHelp" class="form-text text-muted">The altitude of the waypoint. In
                         meters.</small>
                 </div>
